681f8fbab79fbe08c696a1fb2e3f13473a655d88
Robert A. Phillips <bob.phillips@systechone.com>
2022-10-10 06:51:30 -0400

    Body post, search, retrieval working

--- batch_processing/urls.py
+++ batch_processing/urls.py
@@ -4,10 +4,12 @@
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from batch_processing.views import Upload_Batch_File, Upload_Batch_Body, RetrieveObject
+from batch_processing.views import Upload_Batch_File, Upload_Batch_Body, RetrieveObject, RetrieveObjectArray
 
 urlpatterns = [
     path('file/', Upload_Batch_File.as_view(), name="file"),
     path('body/', Upload_Batch_Body.as_view(), name="body"),
     re_path(r'^object/(?P<object_id>[a-zA-Z0-9]*)/$', RetrieveObject.as_view(), name="object"),
+    path('object_list/', RetrieveObjectArray.as_view(), name="object_list"),
+
 ]
--- batch_processing/views.py
+++ batch_processing/views.py
@@ -77,6 +77,28 @@ class JSONSchemaParser(JSONParser):
         else:
             return data
 
+def validate_json_against_schema(json_data):
+    """
+        JSON schema validation approach from https://richardtier.com/2014/03/24/json-schema-validation-with-django-rest-framework/
+
+    :param json_data:
+    :return:
+    """
+    schema_dict = None
+    try:
+        schema_dict = json.loads(
+            open(os.path.join(BASE_DIR, 'files', 'schema.json')).read()
+
+        )
+        logger.debug(f'Got schema dictionary {schema_dict}')
+    except Exception as e:
+        logger.debug(f'Exception obtaining JSON schema: {e}')  # Should result in a 500 error
+        raise InternalServerError(e)
+    try:
+        jsonschema.validate(json_data, schema_dict)
+    except ValueError as error:
+        raise ClientRequestError(_('JSON does not conform to schema.'))
+
 class IgnoreClientContentNegotiation(BaseContentNegotiation):
     def select_parser(self, request, parsers):
         """
@@ -152,20 +174,20 @@ class Upload_Batch_File(APIView):
             # We may also want to save it temporarily and then delete it
             # on successful completion of our tasks (keeping files triggering failures).
             # Certainly, for debugging purposes, there are advantages to retention
-            form = Json_Doc_Upload_Form(request.POST, request.FILES)
-            if form.is_valid():
-                form.save()
-            else:
-                logger.error('Got no file from form')
-                # We don't have anything good. Complain to the caller.
-                return Response(
-                    _(
-                        'No data found in request. No file data in request.'
-                    ),
-                    status.HTTP_400_BAD_REQUEST
-                )
+            # form = Json_Doc_Upload_Form(request.POST, request.FILES)
+            # if form.is_valid():
+            #     form.save()
+            # else:
+            #     logger.error('Got no file from form')
+            #     # We don't have anything good. Complain to the caller.
+            #     return Response(
+            #         _(
+            #             'No data found in request. No file data in request.'
+            #         ),
+            #         status.HTTP_400_BAD_REQUEST
+            #     )
 
-            #file_obj = request.FILES.get("file", None)
+            file_obj = request.FILES.get("file", None)
             if file_obj:
                 # Note -- this assumes that we are guaranteed to be able to read the file data into
                 # memory without issue.  That MAY not be the case, in which case, more robust file
@@ -189,6 +211,31 @@ class Upload_Batch_File(APIView):
                 ),
                 status.HTTP_400_BAD_REQUEST
             )
+        try:
+            validate_json_against_schema(json.loads(batch_data))
+        except ClientRequestError:
+            return Response(
+                _(
+                    'JSON data does not conform to schema.'
+                ),
+                status.HTTP_400_BAD_REQUEST
+            )
+        except InternalServerError:
+            return Response(
+                _(
+                    'Unexpected problem validating JSON in request.'
+                ),
+                status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        except Exception as e:
+            logger.error(f'Unexpected error validating JSON: {e}')
+            return Response(
+                _(
+                    'Unexpected problem processing request.'
+                ),
+                status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
 
         # DO STUFF
         return Response(status.HTTP_200_OK)
@@ -277,18 +324,45 @@ class Upload_Batch_Body(APIView):
                 ),
                 status.HTTP_400_BAD_REQUEST
             )
-	# We have a dictionary. It should conform to schema.  Populate objects
+        try:
+            validate_json_against_schema(batch_dict)
+        except ClientRequestError:
+            return Response(
+                _(
+                    'JSON data does not conform to schema.'
+                ),
+                status.HTTP_400_BAD_REQUEST
+            )
+        except InternalServerError:
+            return Response(
+                _(
+                    'Unexpected problem validating JSON in request.'
+                ),
+                status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        except Exception as e:
+            logger.error(f'Unexpected error validating JSON: {e}')
+            return Response(
+                _(
+                    'Unexpected problem processing request.'
+                ),
+                status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+	    # We have a dictionary. It should conform to schema.  Populate objects
         try:
             batch = Batch(batch_identifier = batch_dict['batch_id'])
+            logger.debug(f'Created batch {batch}')
             batch.save()
             batch_objects = []
             for element in batch_dict['objects']:
                 batch_object = Batch_Object(object_identifier = element['object_id'], batch = batch)
+                logger.debug(f'Created batch_object {batch_object}')
                 batch_object.save()
                 batch_objects.append(batch_object)
                 data_objects = []
                 for item in element['data']:
                     data_object = Batch_Object_Data_Item(key = item['key'], value = item['value'], object = batch_object)
+                    logger.debug(f'Created data_object {data_object}')
                     data_object.save()
                     data_objects.append(data_object)
             return Response(status.HTTP_200_OK)
@@ -332,22 +406,19 @@ class RetrieveObject(APIView):
         try:
             batch_object_data_items = Batch_Object_Data_Item.objects.filter(object_id=batch_object.id)
         except Batch_Object_Data_Item.DoesNotExist:
-            logger.error(f'Failed to find data for object with ID {object_id}')
-            return Response(
-                _("The request object data was not found in the database."),
-                status.HTTP_404_NOT_FOUND
-            )
-        
+            logger.warning(f'Possibly benign: Failed to find data for object with ID {object_id}')
+
+
         # And end
         try:
             batch_object_dict = {}
             batch_object_dict['object_id'] =  batch_object.object_identifier
-            batch_object_dict['objects'] = []
+            batch_object_dict['data'] = []
             for batch_object_data_item in batch_object_data_items:
                 dict_item = {}
                 dict_item['key'] = batch_object_data_item.key
                 dict_item['value'] = batch_object_data_item.value
-                batch_object_dict['objects'].append(dict_item)
+                batch_object_dict['data'].append(dict_item)
             return Response(batch_object_dict,status.HTTP_200_OK )
         except Exception as e:
             logger.error(f'Unexpected problem assembling JSON return: {e}')
@@ -355,3 +426,91 @@ class RetrieveObject(APIView):
                 _("The server failed while processing the request."),
                 status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+
+class RetrieveObjectArray(APIView):
+    """
+    Retrieves an array of objects by key or value.
+    The easiest way is to use query parameters on a GET method, but that is a problematic approach
+    in that the query parameters are exposed as part of the URL. Every Tom, Dick, and Harry sniffing
+    packets knows what you are looking for.  If that isn't a problem, query parameters on a GET method
+    are an easy approach.  BUT, if you want protection, you need to rely on the HTTPS encryption of the body,
+    and use a POST (or a PUT -- arguments can be made either way) method with the value and/or key in the body
+    And for the sake of implementation, I'm just going to support a single key and/or value.  Yeah, it's lazy, but
+    if I don't impose that limit, I need to add logic for AND and OR operations, and that probably exceeds the scope
+    of this little exercise.
+    """
+
+    def get(self, request, object_id=None):
+
+        key = request.GET.get("key", None)
+        value = request.GET.get("value", None)
+        logger.debug(f'Got key {key} and value {value}')
+
+        data_objects = None
+        try:
+            # Yes, for a more complex example, I'd use the Query language and just pass Query Expressions
+            if (key and value):
+                data_objects = Batch_Object_Data_Item.objects.filter(key=key, value=value)
+            elif (key):
+                data_objects = Batch_Object_Data_Item.objects.filter(key=key)
+            elif (value):
+                data_objects = Batch_Object_Data_Item.objects.filter(value=value)
+            else:
+                data_objects = Batch_Object_Data_Item.objects.all()
+
+        except Batch_Object_Data_Item.DoesNotExist:
+            logger.error(f'Failed to find object with ID {object_id}')
+            return Response(
+                _("The request object was not found in the database."),
+                status.HTTP_404_NOT_FOUND
+            )
+        batch_objects=set()
+        try:
+            for data_object in data_objects:
+                batch_objects.add(data_object.object)
+        except Exception as e:
+            logger.error(f'Unexpected exception gathering object IDs: {e}')
+            return Response(
+                _("Problem retrieving related information from database."),
+                status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        # batch_objects = None
+        # try:
+        #     batch_objects = Batch_Object.objects.filter(object__in=objects)
+        # except Exception as e:
+        #     logger.error(f'Unexpected exception gathering objects: {e}')
+        #     return Response(
+        #         _("Problem retrieving related information from database."),
+        #         status.HTTP_500_INTERNAL_SERVER_ERROR
+        #     )
+
+        # And end
+        batch_object_array = []
+        for batch_object in batch_objects:
+            logger.debug(f'Checking object {batch_object.object_identifier}')
+            try:
+                batch_object_dict = {}
+                batch_object_dict['object_id'] = batch_object.object_identifier
+                batch_object_dict['data'] = []
+                batch_object_data_items = None
+                try:
+                    batch_object_data_items = Batch_Object_Data_Item.objects.filter(
+                        object=batch_object)
+                except Batch_Object_Data_Item.DoesNotExist:
+                    logger.warning(
+                        f'Possibly benign: Failed to find data for object with ID {batch_object.id}')
+                for batch_object_data_item in batch_object_data_items:
+                    dict_item = {}
+                    dict_item['key'] = batch_object_data_item.key
+                    dict_item['value'] = batch_object_data_item.value
+                    batch_object_dict['data'].append(dict_item)
+                batch_object_array.append(batch_object_dict)
+            except Exception as e:
+                logger.error(f'Unexpected problem assembling JSON return: {e}')
+                return Response(
+                    _("The server failed while processing the request."),
+                    status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+        return Response(batch_object_array, status.HTTP_200_OK)
+
